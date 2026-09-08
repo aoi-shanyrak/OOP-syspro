d817df256ae4bfbbee5f4b007bff2177c0d0d76c
aoi-shanyrak <aoi-shanyrak@users.noreply.github.com>
2026-09-08 05:23:27 +0000

    Deploying to gh-pages from @ aoi-shanyrak/OOP-syspro@cdb7ba72cbdc6d5d55ac6c878a3021ae57fa9baf 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org","l":"Heapsort"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org","l":"Heapsort"},{"p":"org","l":"Main"}];updateSearchResults();
